1d2b0234cb1495d1cb8ee188608f93568af0ab96
pratiga <gaitrisanu2468@gmail.com>
2022-10-20 22:03:30 +0545

    about page
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../Styles/about.css'
+
+const About = () => {
+  return (
+    <div class="f-box">
+<div class="intro">
+<h1 class="title">
+    Our long-standarding experience
+</h1>
+<p class="des">Founded in the Nepa; in 2065, rental is one of the
+world's leading property agents. our experience and expertise spans the globe, with 600 offices across the americas, Europe,Asia Pacific,Africa and the Middle East.
+
+our dcale give us wide-ranging specialist and local knowledge and we take pride in providing best-in-class advice as we helps indviduals, business and institutions
+ make better property decsions.</p>
+</div>
+
+<div class="book-valuation">
+<div class="left-box">
+<div class="details">
+    <h1 class="title">Book ad valuation</h1>
+    <p class="description">Bol your appointment wit dus today and let us helps you 
+    keep moving . our virtual options are still avialable if you prefer.</p>
+    <span class="find-me">arrange your valuation now </span>
+</div>
+</div>
+<div class="right-box">
+<div class="image-box">
+<img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2621/KvgbdmY6MvPdLaa3dyVNGOw1.png" alt="flower"/>
+</div>
+</div>
+</div>
+    </div>
+  )
+}
+
+export default About
