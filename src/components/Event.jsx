28d355c4ac5afe40216dfd5179ef5e27fa6a3c52
pratiga <gaitrisanu2468@gmail.com>
2022-10-20 22:04:09 +0545

    add more item in event
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import '../Styles/event.css'
-import StarIcon from '@mui/icons-material/Star';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
+import "../Styles/event.css";
+import StarIcon from "@mui/icons-material/Star";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
 import { Link } from "react-router-dom";
 const Event = () => {
   const [event, setEvent] = useState([]);
@@ -21,40 +21,61 @@ const Event = () => {
       });
   }, []);
   return (
-    <>
+    <div className="col-large-12">
+      <div className="col-large-9">
+      
+        {event.map((value) => {
+          return (
+            <Link to={`/eventDetail/${value.id}`}>
+              <div className="containers">
+                <div className="image-event">
+                  <img src={value.image_url} alt="images" />
+                </div>
+                <div className="row-1">
+                  <h2 className="name-event">{value.name}</h2>
+                  <p className="short-desc">{value.short_description}</p>
+                </div>
+                <div className="row-2"></div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
       <div className="Event-collection">
         <h1 className="title">list of Events</h1>
         {event.map((value) => {
           return (
             <>
               <div className="list-event">
-              <Link to={`/eventDetail/${value.id}`}>
-                <div className="image">
-                  <img src={value.image_url} alt="images" />
-                </div>
+                <Link to={`/eventDetail/${value.id}`}>
+                  <div className="image">
+                    <img src={value.image_url} alt="images" />
+                  </div>
                 </Link>
                 <div className="details">
-                  <span className="name">{value.name}</span><br/>
-                  <span className="location">{value.location}</span><br/>
+                  <span className="name">{value.name}</span>
+                  <br />
+                  <span className="location">{value.location}</span>
+                  <br />
                   <span className="date">Date: {value.date}</span>
                 </div>
                 <div className="icons">
-                  <span><StarIcon/></span>
-                  <span><WatchLaterIcon/></span>
-                  <span><AutorenewIcon /></span>
+                  <span>
+                    <StarIcon />
+                  </span>
+                  <span>
+                    <WatchLaterIcon />
+                  </span>
+                  <span>
+                    <AutorenewIcon />
+                  </span>
                 </div>
               </div>
             </>
           );
         })}
-        <div>
-          <h1>loremThiago from Brazil shares how our Computer Professionals Master's helped him get into the Computer Vision field:
-"MIU provides all the tools and resources needed for a successful IT career. Their financial aid makes costs affordable, and the career preparation is excellent. I made valuable friends for many countries and have learned greatly from your experience. I had excellent professors in career advisors who have supported me in improving both my technical and soft skills leading to mThiago from Brazil shares how our Computer Professionals Master's helped him get into the Computer Vision field:
-"MIU provides all the tools and resources needed for a successful IT career. Their financial aid makes costs affordable, and the career preparation is excellent. I made valuable friends for many countries and have learned greatly from your experience. I had excellent professors in career advisors who have supported me in improving both my technical and soft skills leading to mThiago from Brazil shares how our Computer Professionals Master's helped him get into the Computer Vision field:
-"MIU provides all the tools and resources needed for a successful IT career. Their financial aid makes costs affordable, and the career preparation is excellent. I made valuable friends for many countries and have learned greatly from your experience. I had excellent professors in career advisors who have supported me in improving both my technical and soft skills leading to m</h1>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
