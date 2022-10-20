ad58f66c28211d2be183157bc28916223293a1ba
pratiga <gaitrisanu2468@gmail.com>
2022-10-20 22:04:45 +0545

    slider in list of event
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "../Styles/home.css";
+import "../Styles/slider.scss";
 import axios from "axios";
 import StarIcon from "@mui/icons-material/Star";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
-import CircleIcon from "@mui/icons-material/Circle";
-import BlurCircularIcon from '@mui/icons-material/BlurCircular';
+import BlurCircularIcon from "@mui/icons-material/BlurCircular";
 import SearchIcon from "@mui/icons-material/Search";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
@@ -23,12 +23,76 @@ const Home = () => {
         console.log(err);
       });
   }, []);
+
+  const slider = React.useRef(null);
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      >
+        NEXT
+      </div>
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      >
+        BACK
+      </div>
+    );
+  }
   const settings = {
-    dots: true,
+    dots: false,
+    arrows: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 1424,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          arrows: false,
+          dots: false,
+        },
+      },
+
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          arrows: false,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          initialSlide: 2,
+        },
+      },
+    ],
+
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <>
@@ -43,7 +107,7 @@ const Home = () => {
             <div class="b-type">
               <BlurCircularIcon />
               <span>Event</span>
-              <BlurCircularIcon/>
+              <BlurCircularIcon />
               <span>Sponcer</span>
             </div>
             <div class="s-button">
@@ -58,46 +122,60 @@ const Home = () => {
             </div>
           </div>
         </div>
-        
-      
-        
+
         <div className="latest-event">
-          <h3>Latest Events</h3>
-          <button>View All Property</button>
+          <h1 className="event-title">Latest Events</h1>
+          <button className=" btn event-button">View All Property</button>
         </div>
 
         <div className="event">
-        <Slider {...settings}>
-          {event.map((value) => {
-            return (
-              <div className="event-list">
-                <Link to={`/eventDetail/${value.id}`}>
-                  <div className="images">
-                    <img src={value.image_url} alt="images" />
+          
+          <Slider ref={slider} {...settings}>
+            {event.map((value) => {
+              return (
+                <div className="event-list">
+                  <Link to={`/eventDetail/${value.id}`}>
+                    <div className="images">
+                      <img src={value.image_url} alt="images" />
+                    </div>
+                  </Link>
+                  <div className="detail">
+                    <span className="name">{value.name}</span>
+                    <br />
+                    <span className="location">{value.location}</span>
+                    <br />
+                    <span className="date">Date: {value.date}</span>
                   </div>
-                </Link>
-                <div className="detail">
-                  <span className="name">{value.name}</span>
-                  <br />
-                  <span className="location">{value.location}</span>
-                  <br />
-                  <span className="date">Date: {value.date}</span>
+                  <div className="icon">
+                    <span>
+                      <StarIcon />
+                    </span>
+                    <span>
+                      <WatchLaterIcon />
+                    </span>
+                    <span>
+                      <AutorenewIcon />
+                    </span>
+                  </div>
                 </div>
-                <div className="icon">
-                  <span>
-                    <StarIcon />
-                  </span>
-                  <span>
-                    <WatchLaterIcon />
-                  </span>
-                  <span>
-                    <AutorenewIcon />
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
           </Slider>
+          <div style={{ margin: 20 }}>
+            <button
+              className=" btns prev-button"
+              onClick={() => slider?.current?.slickPrev()}
+            >
+              Prev
+            </button>
+            <button
+            className=" btns next-button"
+              style={{ marginLeft: 20 }}
+              onClick={() => slider?.current?.slickNext()}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </>
