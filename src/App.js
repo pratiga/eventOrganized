be30a07e10b6b7445449907398a38a6c65a60855
pratiga <gaitrisanu2468@gmail.com>
2022-11-15 19:19:33 +0545

    add route
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Headers from "./pages/header";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Event from "./components/Event";
 import EventForm from "./pages/eventForm";
 import EventDetail from "./components/eventDetail";
+import EditEventForm from "./pages/editEventForm";
+import Service from "./components/service";
 
 function App() {
   return (
@@ -16,9 +18,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
         <Route path="/event" element={<Event />} />
         <Route path="/form" element={<EventForm />} />
+        <Route path ="/editForm/:id" element={<EditEventForm/>} />
         <Route path="/eventDetail/:id" element={<EventDetail />}></Route>
+        <Route path="/eventDetail/:id" element={ <Navigate to="/event" /> } />
       </Routes>
     </BrowserRouter>
   );
