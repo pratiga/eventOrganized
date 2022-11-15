017b656bec980b67f5bb7f30172485d534c69003
pratiga <gaitrisanu2468@gmail.com>
2022-11-15 19:17:04 +0545

    add services
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/data/services.jsx
@@ -0,0 +1,38 @@
+  export const services = [
+     {
+         title:"Conferences and venue sourcing",
+         short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+         description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+         
+     },
+     {
+        title:"Conferences and venue sourcing",
+        short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+        description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+        
+    },
+    {
+        title:"Conferences and venue sourcing",
+        short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+        description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+        
+    },
+    {
+        title:"Conferences and venue sourcing",
+        short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+        description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+        
+    },
+    {
+        title:"Conferences and venue sourcing",
+        short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+        description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+        
+    }, {
+        title:"Conferences and venue sourcing",
+        short_text:"Event planning companies can work with you to create a unique conference identity with careful programming ",
+        description:"Professional event managers are skilled in the technical as well as the creative and logistical implementation of events. Whether it’s a trade show, product launch, wedding, cocktail party, awards ceremony or a sit-down gala dinner, event managers first identify the audience of each event and go out of the way to understand your brand and budget as well."
+        
+    }
+     
+ ]
